222d0b399784a4c4526e3db8bbed02901af1001e
Adrian Deardorff <80deardorff@live.com>
2018-07-27 04:21:46 -0700

    Added status barfor sales.

--- js/main.js
+++ js/main.js
@@ -4,8 +4,8 @@ var user = [{
     lastName: 'Deardorff',
     idNum: '',
     img: 'img/adrianDeardorff.png',
-    monthTarget: '',
-    monthSales: ''
+    target: 1208065,
+    sales: 865475
 }]
 
 // GENERATING A 4 DIGIT RANDOM ID NUMBER
@@ -27,31 +27,63 @@ $('#userImg').attr('src', user[0].img);
 $('#userName').text(user[0].firstName + ' ' + user[0].lastName);
 $('#userId').text('Employee ID: ' + user[0].idNum);
 
+// TIME VARIABLES
+var now, year, month, day, hour, minute, second, meridiem;
+var monthNames = ["January", "February", "March", "April", "May", "June",
+"July", "August", "September", "October", "November", "December"];
+
 // FUNCTION FOR DISPLAYING TIME
 function currentTime() {
-    var now = new Date();
-    var year = now.getFullYear();
-    var monthNames = ["January", "February", "March", "April", "May", "June",
-    "July", "August", "September", "October", "November", "December"];
-    var month = monthNames[now.getMonth()];
-    var day = now.getDate();
-    var hour = formatAddZero(formatToStandardTime(now.getHours()));
-    var minute = formatAddZero(now.getMinutes());
-    var second = formatAddZero(now.getSeconds());
-    var meridiem = (function() {
+    now = new Date();
+    year = now.getFullYear();
+    month = monthNames[now.getMonth()];
+    day = now.getDate();
+    hour = formatAddZero(formatToStandardTime(now.getHours()));
+    minute = formatAddZero(now.getMinutes());
+    second = formatAddZero(now.getSeconds());
+    meridiem = (function() {
                         if (now.getHours() > 12) {
                             return 'PM';
                         } else {
                             return 'AM';
                         }
                     })();
-// SALES INFO
+    // CURRENT TIME
     $('#time').text(month + ' ' + day + ', ' + year + ' ' + hour + ':' + minute + ':' + second + ' ' + meridiem);
     setTimeout(function() {
         currentTime();
     }, 1000)
 };
 currentTime();
+
+// SALES VARIABLES
+var target = user[0].target;
+var sales = user[0].sales;
+var diff = target - sales;
+
+// MONTHLY TARGET
+$('#monthTarget').text(month + ' Target: $' + target.toLocaleString(undefined, {maximumFractionDigits:0}));
+
+// CURRENT SALES
+$('#currentSales').text(month + ' Sales: $' + sales.toLocaleString(undefined, {maximumFractionDigits:0}));
+
+// STATUS FOR TIME OF MONTH
+var span1 = document.createElement('span');
+span1.innerText = (diff / target * 100).toLocaleString(undefined, {maximumFractionDigits:2}) + '%';
+span1.setAttribute('id', 'span1');
+$('#currentSales').append(span1);
+
+// SALES BAR BASED ON PERCENT OF TARGET FOR MONTH
+var monthPercent = Math.floor((sales / target * 100)).toLocaleString(undefined, {maximumFractionDigits:0});
+if (monthPercent > 100) {
+    monthPercent = 100;
+};
+$('#salesBar').css({'width': monthPercent + '%', 'max-width': '100%', 'transition': 'width 3.0s ease-in-out'});
+$('#indicator').html('<i class="fas fa-caret-up"></i><br>' + monthPercent + '%');
+if (monthPercent < 3) {
+    monthPercent = 3;
+}
+$('#indicator').css({'left': monthPercent - 3 + '%', 'transition': 'left 3.0s ease-in-out'})
 
 // FUNCTION FOR CHANGING FROM MILITARY TIME TO STANDARD TIME
 function formatToStandardTime(hour) {
@@ -70,3 +102,17 @@ function formatAddZero(num) {
         return num;
     }
 };
+
+// FUNCTION FOR COUNTING NUMBER OF WORKING DAYS IN MONTH
+// function isWeekday(year, month, day) {
+// var day = new Date(year, month, day).getDay();
+// return day !=0 && day !=6;
+// }
+// function getWeekdaysInMonth(month, year) {
+// var days = daysInMonth(month, year);
+// var weekdays = 0;
+// for(var i=0; i< days; i++) {
+//     if (isWeekday(year, month, i+1)) weekdays++;
+// }
+// return weekdays;
+// }
